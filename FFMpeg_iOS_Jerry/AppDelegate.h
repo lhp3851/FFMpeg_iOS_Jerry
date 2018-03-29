0279cd7b99f9037d8d79e6e513fc981610af91d9
liuhepeng <liuhepeng@besttmkj.com>
2018-03-29 18:34:42 +0800

    Initial Commit

---
+++ FFMpeg_iOS_Jerry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFMpeg_iOS_Jerry
+//
+//  Created by Jerry on 29/03/2018.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFMpeg_iOS_Jerry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFMpeg_iOS_Jerry
+//
+//  Created by Jerry on 29/03/2018.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
