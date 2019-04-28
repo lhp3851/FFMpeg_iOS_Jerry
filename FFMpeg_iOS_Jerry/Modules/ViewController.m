daa77dbf3467b007331e8b68c5c330f81730a636
lhp3851 <lhp3851@163.com>
2019-04-28 18:43:10 +0800

    [feature][多线程]:

--- FFMpeg_iOS_Jerry/Modules/ViewController.m
+++ FFMpeg_iOS_Jerry/Modules/ViewController.m
@@ -18,6 +18,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
    
+    
+    dispatch_queue_t q = dispatch_queue_create("dantesx", DISPATCH_QUEUE_CONCURRENT);
+    void (^task)(void) = ^ {
+        // 1. 用户登录，必须要第一个执行
+        dispatch_sync(q, ^{
+            NSLog(@"用户登录 %@", [NSThread currentThread]);
+        });
+        // 2. 扣费
+        dispatch_async(q, ^{
+            NSLog(@"扣费 %@", [NSThread currentThread]);
+        });
+        // 3. 下载
+        dispatch_async(q, ^{
+            NSLog(@"下载 %@", [NSThread currentThread]);
+        });
+    };
+    NSLog(@"前 %@", [NSThread currentThread]);
+    dispatch_async(q, task);
+    NSLog(@"后 %@", [NSThread currentThread]);
+    NSLog(@"董铂然 come here");
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        printf("sdsfdsffs");
+    });
 }
 
 
