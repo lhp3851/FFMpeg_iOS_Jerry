197d8d4dab1d9e54ef78a83d61015665319a39b4
Jerry <lhp3851@163.com>
2019-05-14 20:34:14 +0800

    [feature][网络]1、IP获取

---
+++ FFMpeg_iOS_Jerry/Tool/HTTPTool/IP/KKIPTool.h
@@ -0,0 +1,17 @@
+//
+//  KKIPTool.h
+//  FFMpeg_iOS_Jerry
+//
+//  Created by lhp3851 on 2018/12/6.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KKIPTool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FFMpeg_iOS_Jerry/Tool/HTTPTool/IP/KKIPTool.m
@@ -0,0 +1,13 @@
+//
+//  KKIPTool.m
+//  FFMpeg_iOS_Jerry
+//
+//  Created by lhp3851 on 2018/12/6.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+#import "KKIPTool.h"
+
+@implementation KKIPTool
+
+@end
